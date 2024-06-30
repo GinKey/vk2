55ca9a31fb9f438f3880651174986fb8bfc5a156
andreyzharov <monotank23@gmail.com>
2024-06-30 11:52:50 +0300

    update api create new functions

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+// Logo.js
+import React from 'react';
+import './logo.css';
+
+const Logo = () => {
+    return (
+        <div className="logo">VK CINEMA</div>
+    );
+};
+
+export default Logo;
