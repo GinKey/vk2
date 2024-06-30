4a424be94d9c356ebca96494736c034e64c2f5dd
andreyzharov <monotank23@gmail.com>
2024-06-30 20:15:25 +0300

    mobile update

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -32,11 +32,10 @@ const MovieDetails = () => {
             <div className="movie-info">
                 <h2>{movie.name}</h2>
                 <p className="description">{movie.description}</p>
-                <p><strong>Year:</strong> {movie.year}</p>
-                <p><strong>Rating:</strong> {movie.rating?.kp}</p>
-                <p><strong>Premiere:</strong> {new Date(movie.premiere?.world).toLocaleDateString()}</p>
+                <p><strong>Год:</strong> {movie.year}</p>
+                <p><strong>Рейтинг:</strong> {movie.rating?.kp}</p>
                 <div className="genres">
-                    <strong>Genres: </strong>
+                    <strong>Жанры: </strong>
                     {movie.genres.map((genre) => genre.name).join(', ')}
                 </div>
             </div>
